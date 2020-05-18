155d1d01a40efce890d49081bdc8e8ed81982304
rochellev <rochellevroberts@utexas.edu>
2020-05-18 09:47:30 -0700

    added key and method to add new item on list

--- src/App.js
+++ src/App.js
@@ -3,39 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // when update todos, you need to get copy, modify, then return in setTodos
   const [todos, setTodos] = useState([
     {content: 'put away laundry',
     isCompleted: true,
+    id: 1,
     },
     {
       content: 'vacuum rug',
       isCompleted: false,
+      id: 2,
     },
     {
       content: 'do dishes',
       isCompleted: 'false',
+      id: 3,
     }
   ])
+
+  const [todoCount, setTodoCount] = useState(3);
 
   function handleKeyDown(e, i){
     if(e.key === 'Enter'){
       createTodoAtIndex(e, i);
     }
   }
-  // update todos state with new empty todo item
+  // add empty todo item to todos
   function createTodoAtIndex(e,i){
-
     // copy because state not be directly mutated
+    var newTodoCount = todoCount;
+    newTodoCount+=1;
+
     const newTodos = [...todos];
     newTodos.splice(i+1, 0, {
       content: '',
       isCompleted: false,
+      id: newTodoCount,
     });
+
+    setTodoCount(newTodoCount);
     setTodos(newTodos);
     // wait for state to finish update before focusing on new rendered input
     setTimeout(() => {
       document.forms[0].elements[i + 1].focus();
-    }, 0);
+      }, 0);
+  }
+
+  // update todo list state
+  function updateTodoAtIndex (e, i){
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    console.log("updated item id: " + newTodos[i].id);
+    setTodos(newTodos);
 
   }
 
@@ -49,10 +68,12 @@ function App() {
           {todos.map((todoItem, i) => (
             <div className="todo">
               <div className="checkbox" />
-              <input 
+              <input
+                key={todoItem.id} 
                 type="text" 
                 value={todoItem.content}
                 onKeyDown={e => handleKeyDown(e, i)}
+                onChange={e => updateTodoAtIndex(e, i)}
               />
              </div>
           ))}
