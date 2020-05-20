17ad09da2fa3278d97cf221d768f5649159b2305
rochellev <rochellevroberts@utexas.edu>
2020-05-20 09:47:03 -0700

    remove CheckBox component

--- src/ListItem.js
+++ src/ListItem.js
@@ -4,16 +4,10 @@ import "./App.css";
 function ListItem(props) {
   return (
     <div className={`todo ${props.isCompleted && 'todo-is-completed'}`}>
-      <Checkbox isCompleted={props.isCompleted} />
-      <input type="text" value={props.content}/>
-    </div>
-  );
-}
-
-function Checkbox(props) {
-  return (
-    <div className={'checkbox'}>
+      <div className={'checkbox'}>
       {props.isCompleted && <span>&#x2714;</span>}
+    </div>
+      <input type="text" value={props.content}/>
     </div>
   );
 }
