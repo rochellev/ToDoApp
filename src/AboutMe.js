376cd1511aeff8be8d6cdda741301653aca3691f
rochellev <rochellevroberts@utexas.edu>
2020-05-26 09:30:28 -0700

    use vh and vw for app padding to look better on mobile and edit about me blurb

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,7 +4,7 @@ import './App.css';
 function AboutMe(){
   return(
     <div className='todo-list'>
-      <p>I developed this To Do App to practice my React and AWS skills. To begin I followed <a href="https://github.com/rochellev/ToDoApp">this</a> tutorial to make a static version of the app using React functional components and hooks. I will add other features and connect it to a database. You can see the app here, rochellecrain.com.</p>
+      <p>I developed this To Do App to practice my React and AWS skills. Source code and more information about my development process <a href="https://github.com/rochellev/ToDoApp">here</a>.</p>
     </div>
   )
 }
