bcbbaddb56a8a6d9c1275297bb1135e80e9c3bb4
rochellev <rochellevroberts@utexas.edu>
2020-06-01 15:55:19 -0700

    add keyOrderedList to render in order, working on it but am switching branch to try something else first

--- src/List.js
+++ src/List.js
@@ -3,30 +3,45 @@ import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import ListItem from "./ListItem";
 
-
-
 function List() {
-  
+  const KEY_LIST_KEY = "keyOrderedList";
+  // keep order of todo list
+
   // runs when component is initialized, get todos from local storage
-  const [todos, setTodos] = useState( () => {
+  const [todos, setTodos] = useState(() => {
     const restoredList = [];
     const keys = Object.keys(localStorage);
-    console.log(`keys.length = ${keys.length}`);
     if (keys.length === 0) {
-      restoredList.push({ content: "", isCompleted: false, id: uuidv4() });
+      const newId = uuidv4();
+      const keyArray = [newId];
+      restoredList.push({ content: "", isCompleted: false, id: newId });
+     
     }
-      for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      if (key != KEY_LIST_KEY) {
         restoredList.push(JSON.parse(localStorage.getItem(key)));
+      }
     }
     return restoredList;
   });
 
-  useEffect( () => {
+  const [keyOrderedList, setKeyOrderedList] = useState(() => {
+    const newKeyOrderedList = [];
+    newKeyOrderedList.push(todos[0].id);
+    return newKeyOrderedList;
+  });
+
+  useEffect(() => {
     todos.forEach(item => {
       localStorage.setItem(JSON.stringify(item.id), JSON.stringify(item));
     });
   }, [todos]);
+
+  useEffect(() => {
+    localStorage.setItem(KEY_LIST_KEY, JSON.stringify(keyOrderedList))
+  })
+
 
   // event handlers
   function handleKeyDown(e, i) {
@@ -41,12 +56,14 @@ function List() {
   // add empty todo item to todos
   function createTodoAtIndex(e, i) {
     // copy because state not be directly mutated
+    const newId = uuidv4();
     const newTodos = [...todos];
     newTodos.splice(i + 1, 0, {
       content: "",
       isCompleted: false,
-      id: uuidv4()
+      id: newId
     });
+  
     setTodos(newTodos);
     // wait for state to finish update before focusing on new rendered input
     setTimeout(() => {
@@ -62,14 +79,16 @@ function List() {
 
   function removeTodoAtIndex(i) {
     if (i === 0 && todos.length === 1) return;
-    console.log(`typeof= ${typeof todos[i].id}`)
+    console.log(`typeof= ${typeof todos[i].id}`);
     localStorage.removeItem(JSON.stringify(todos[i].id));
     setTodos(todos =>
       todos.slice(0, i).concat(todos.slice(i + 1, todos.length))
     );
-    
+
     setTimeout(() => {
-      (i === 0) ? document.forms[0].elements[0].focus() : document.forms[0].elements[i-1].focus();
+      i === 0
+        ? document.forms[0].elements[0].focus()
+        : document.forms[0].elements[i - 1].focus();
     }, 0);
   }
 
@@ -83,16 +102,14 @@ function List() {
     <form className="todo-list">
       <ul>
         {todos.map((todoItem, i) => (
-        
-            <ListItem
-              key={todoItem.id}
-              content={todoItem.content}
-              isCompleted={todoItem.isCompleted}
-              onKeyDown={e => handleKeyDown(e, i)}
-              onChange={e => updateTodoAtIndex(e, i)}
-              onClick={e => toggleTodoCompleteAtIndex(i)}
-            />
-        
+          <ListItem
+            key={todoItem.id}
+            content={todoItem.content}
+            isCompleted={todoItem.isCompleted}
+            onKeyDown={e => handleKeyDown(e, i)}
+            onChange={e => updateTodoAtIndex(e, i)}
+            onClick={e => toggleTodoCompleteAtIndex(i)}
+          />
         ))}
       </ul>
     </form>
