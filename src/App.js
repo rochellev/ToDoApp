2cbc2eb58faffa971502910361e330b3663149e9
rochellev <rochellevroberts@utexas.edu>
2020-06-15 10:28:36 -0700

    figured out how to add event handlers, need a distance prop so the click event is not swallowed immediately by the sortable element HOC

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const SortableGifsContainer = sortableContainer(({ children }) => (
   <div >{children}</div>
 ));
 
-const SortableGif = sortableElement(({ gif }) => <Gif key={gif} gif={gif} />);
-
+// const SortableGif = sortableElement(({ gif, onGifClick }) => <Gif key={gif} gif={gif} onGifClick={onGifClick} />);
+const SortableGif = sortableElement(({ ...props }) => <Gif key={props.gif} gif={props.gif} onClick={props.onClick} />);
 function App() {
   const [gifs, setGifs] = useState([
     "https://media.giphy.com/media/3ohhwoWSCtJzznXbuo/giphy.gif",
@@ -21,8 +21,14 @@ function App() {
     "https://media.giphy.com/media/3ohzgD1wRxpvpkDCSI/giphy.gif",
     "https://media.giphy.com/media/xT1XGYy9NPhWRPp4pq/giphy.gif"
   ]);
+  
 
   const onSortEnd = ({ oldIndex, newIndex }) => setGifs(arrayMove(gifs, oldIndex, newIndex));
+
+  function onGifClick(){
+    console.log("oh hello click");
+    alert("hello");
+  }
 
   return (
     <div className="app">
@@ -33,15 +39,17 @@ function App() {
       <AboutMe />
       <h1>Drag those GIFs around</h1>
       <h2>Set 1</h2>
-      <SortableGifsContainer axis="y" onSortEnd={onSortEnd}>
+      <SortableGifsContainer axis="y" distance="5" onSortEnd={onSortEnd}>
         {gifs.map((gif, i) => (
           <SortableGif
             index={i}
             key={gif}
             gif={gif}
+            onClick={e => onGifClick()}
           />
         ))}
       </SortableGifsContainer>
+      
     </div>
   );
 }
