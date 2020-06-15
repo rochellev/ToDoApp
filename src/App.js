3dae9d011ceb3ba6d5d3d5c678935273c93da803
rochellev <rochellevroberts@utexas.edu>
2020-06-15 10:33:57 -0700

    fix a few more errors i missed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
@@ -7,28 +6,10 @@ import AboutMe from "./AboutMe";
 import Gif from "./Gif";
 import { sortableContainer, sortableElement } from "react-sortable-hoc";
 import arrayMove from "array-move";
-=======
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import List from './List';
-import AboutMe from './AboutMe';
-import Gif from './Gif';
-import { sortableContainer, sortableElement } from 'react-sortable-hoc';
-import arrayMove from 'array-move';
-
->>>>>>> 532e2fc6a1ec3931c7e36729955e3812564ccfd0
 
 // this might go in List for todo proj
 const SortableGifsContainer = sortableContainer(({ children }) => <div className="gifs">{children}</div>);
 
-// move the part with mapping a each todoItem to component
-// wrapping each ListItem in a HOC sortableConponent
-const SortableGif = sortableElement(({ gif }) => <Gif key={gif} gif={gif} />);
-
-const SortableGifsContainer = sortableContainer(({ children }) => (
-  <div >{children}</div>
-));
 
 // const SortableGif = sortableElement(({ gif, onGifClick }) => <Gif key={gif} gif={gif} onGifClick={onGifClick} />);
 const SortableGif = sortableElement(({ ...props }) => <Gif key={props.gif} gif={props.gif} onClick={props.onClick} />);
