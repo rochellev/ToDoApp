86bba7d65d7e173ce5ae0c1b00e260e7581db7f1
rochellev <rochellevroberts@utexas.edu>
2020-06-22 10:48:19 -0700

    remove unused component

--- src/SortableListContainer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { sortableContainer, sortableElement } from 'react-sortable-hoc';
-import arrayMove from 'array-move';
-import List from "./List";
-
-const SortableListContainer = sortableContainer(({children}) => {return <List>{children}</List>});
-
