5d9bf026d300bcad706dcb2cb9d287459ddb1ef5
rochellev <rochellevroberts@utexas.edu>
2020-06-22 11:02:09 -0700

    add test for rendering app without crashing

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// import React from 'react';
+// import ReactDOM from 'react-dom'
+// import App from './App';
+
+test('renders app without crashing', () => {
+  const div = document.createElement('div');
+  render(<App />, div)
 });
