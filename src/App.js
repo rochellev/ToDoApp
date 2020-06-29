0f673812caf715ef9ee947552a1abe897c389a45
rochellev <rochellevroberts@utexas.edu>
2020-06-29 14:07:08 -0700

    add todo cards state to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import List from "./List";
 import AboutMe from "./AboutMe";
 
 function App() {
+  // list of todo lists
+  const [todoCards, setTodoCards] = useState( () => {
+    
+  });
+
   return (
     <div className="app">
       <div className="header">
