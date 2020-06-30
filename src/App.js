ad8dedf92bf697d74ffc3b0ebcd39cf944773c67
rochellev <rochellevroberts@utexas.edu>
2020-06-30 09:20:26 -0700

    add redux

--- src/App.js
+++ src/App.js
@@ -5,12 +5,35 @@ import List from "./components/List";
 import AboutMe from "./components/AboutMe";
 
 function App() {
+
+  const [todos, setTodos] = useState(() => {
+    var restoredList = JSON.parse(localStorage.getItem("todoList"));
+
+    if (!restoredList) {
+      // make this a 2d array list of todo lists
+      restoredList = [
+        { content: "", isCompleted: false, id: uuidv4(), isFocused: false }
+      ];
+    }
+    return restoredList;
+  });
+
+  /*
+  setTodos((prevState) => newState);
+
+  const setTodosAt0 = (todos) => setTodos(setState with prior state  (prevState) => [todos, prevState[1]]);
+
+
+  */
+
   return (
     <div className="app">
       <div className="header">
         <img src={logo} className="logo" alt="logo" />
       </div>
+      // todos[0]
       <List />
+      // todos[1]
       <AboutMe />
     </div>
   );
