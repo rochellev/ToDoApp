db2c10efa9225ea1d9ee77c579dba5acc25cdeac
rochellev <rochellevroberts@utexas.edu>
2020-06-30 10:05:11 -0700

    add constant lists, for dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import logo from "./icons/logo.svg";
 import "./App.css";
 import List from "./components/List";
 import AboutMe from "./components/AboutMe";
 
+const myTodos = [
+  [
+    {
+      content: "my list 1",
+      isCompleted: false,
+      id: uuidv4(),
+      isFocused: false
+    },
+    { content: "dishes", isCompleted: false, id: uuidv4(), isFocused: false },
+    {
+      content: "water lemon tree",
+      isCompleted: true,
+      id: uuidv4(),
+      isFocused: false
+    },
+    { content: "clean mug", isCompleted: false, id: uuidv4(), isFocused: false }
+  ],
+  [
+    {
+      content: "my list 2",
+      isCompleted: false,
+      id: uuidv4(),
+      isFocused: false
+    },
+    {
+      content: "row the rower",
+      isCompleted: false,
+      id: uuidv4(),
+      isFocused: false
+    }
+  ]
+];
 function App() {
   // 2D array
   const [todos, setTodos] = useState(() => {
@@ -28,7 +60,7 @@ function App() {
       <div className="header">
         <img src={logo} className="logo" alt="logo" />
       </div>
-      <List todos={todos}/>
+      <List todos={todos} />
       <AboutMe />
     </div>
   );
